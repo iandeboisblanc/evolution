c29d2f9ddb8bc27786b85c6dcb7f4e06fd80f6b4
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-04-06 20:29:26 -0700

    Refactor client code with helper file to clean up codebase

--- client/evolution.js
+++ client/evolution.js
@@ -5,73 +5,47 @@ var Eves = [];
 //INIT:
 // HTTPRequest.get('http://159.203.249.253/api/state', function(status, headers, content) {
 HTTPRequest.get('http://localhost:3000/api/state', function(status, headers, content) {
-  
   var body = JSON.parse(content);
   settings = body.settings;
   Eves = body.Eves;
 
   renderBoard();
   renderEves();
-  animate();
+
+  setInterval(function() {
+    applyLimbForces();
+    updateBodyPartPositions();
+  }, settings.stepTime);
   
-  setInterval(collectStats, 10000);
   
   setInterval(function() {
     killEves();
     Eves.push(deriveEveData(chooseOne(Eves)));
     renderEves();
   }, settings.killTime);
+
+  setInterval(collectStats, 10000);
 });
 
-
-//helpful things
-var findDistance = function(pos1, pos2) {
-  return Math.sqrt(Math.pow(pos2.x - pos1.x, 2) + Math.pow(pos2.y - pos1.y, 2));
-};
-
-var limitPositions = function(x,y,r) {
-  x = Math.max(r, Math.min(x, (settings.width - r)));
-  y = Math.max(r, Math.min(y, (settings.height - r)));
-  return [x,y];
-};
-
-var chooseOne = function(args) {
-  args = Array.prototype.slice.call(arguments);
-  if(args.length === 1 && typeof Array.isArray(args[0])) {
-    return args[0][randomInt(args[0].length)]
-  }
-  return args[randomInt(args.length)];
-};
-
-var randomInt = function(limit) {
-  return Math.floor(Math.random() * limit);
-};
-
-var getJunctions = function(array) {
-  var possibleConnections = [];
-  for(var i = 0; i < array.length; i++) {
-    for(var j = i + 1; j < array.length; j++) {
-      possibleConnections.push([i,j]);
-    }
-  }
-  return possibleConnections;
-};
-
-var getAvgPosition = function(eve) {
-  var xPos = 0;
-  var yPos = 0;
-  for(var j = 0; j < eve.bodyParts.length; j++) {
-    xPos += eve.bodyParts[j].pos.x;
-    yPos += eve.bodyParts[j].pos.y;
-  }
-  var pos = {
-    x:xPos / eve.bodyParts.length,
-    y:yPos / eve.bodyParts.length,
-  }
-  return pos;
-};
-
 //FUNCTIONS:
+function renderBoard() {
+  d3.select('body').selectAll('svg')
+    .data([{width:settings.width, height:settings.height}])
+    .enter()
+    .append('svg')
+    .attr('class', 'board')
+    .attr('width', settings.width)
+    .attr('height', settings.height);
+}
+
+function renderEves() {
+  var d3Eves = d3.select('.board').selectAll('.eve')
+    .data(Eves, function(d) { return d.id });
+  //new ones:
+  d3Eves.enter()
+    .append(drawEve);
+}
+
 function drawEve(data) {
   var eve = document.createElementNS('http://www.w3.org/2000/svg','g');
   var eveId = 'eve' + data.id;
@@ -106,26 +80,141 @@ function drawEve(data) {
       .attr('id', eveId + 'b' + (i));
   }
 
+  // Apply click handler
   d3.select(eve)
-  .on('click', function(d,i) {
-    d.beingDragged = true;
-    console.log('Stats for eve: ' + d.id + '\n' +
-    'Generation: ' + d.stats.generation + '\n' +
-    'Distance Travelled: ' + d.stats.distanceTraveled + '\n' +
-    'Avg Speed: ' + ((d.stats.distanceTraveled / d.stats.cyclesSinceBirth) || 0));
-});
+    .on('click', function(d,i) {
+      d.beingDragged = true;
+      console.log('Stats for eve: ' + d.id + '\n' +
+      'Generation: ' + d.stats.generation + '\n' +
+      'Distance Travelled: ' + d.stats.distanceTraveled + '\n' +
+      'Avg Speed: ' + ((d.stats.distanceTraveled / d.stats.cyclesSinceBirth) || 0));
+  });
 
   return eve;
 };
 
-function renderBoard() {
-  d3.select('body').selectAll('svg')
-    .data([{width:settings.width, height:settings.height}])
-    .enter()
-    .append('svg')
-    .attr('class', 'board')
-    .attr('width', settings.width)
-    .attr('height', settings.height);
+function applyLimbForces() {
+  for(var i = 0; i < Eves.length; i++) {
+    var eve = Eves[i];
+    for(var j = 0; j < eve.limbs.length; j++) {
+      var limb = eve.limbs[j];
+      var b0 = eve.bodyParts[limb.connections[0]];
+      var b1 = eve.bodyParts[limb.connections[1]];
+      var displacement, force;
+      limb.currentLength = findDistance(b0.pos, b1.pos)
+      if(limb.growing) {
+        displacement = limb.maxLength - limb.currentLength;
+        force = displacement * 0.1 - 1.5;
+        if(limb.currentLength >= limb.maxLength) {
+          limb.growing = false;
+        }
+      } else {
+        displacement = limb.initialLength - limb.currentLength;
+        force = displacement * 0.1 + 1.5;
+        if(limb.currentLength <= limb.initialLength) {
+          limb.growing = true;
+        }
+      }
+      var xPosDiff = b1.pos.x - b0.pos.x;
+      var yPosDiff = b1.pos.y - b0.pos.y;
+      if(xPosDiff === 0) {
+        var theta = Math.PI;
+      }
+      else {
+        var theta = Math.atan(yPosDiff / xPosDiff);
+      }
+      if (xPosDiff >= 0) {
+        force *= -1;
+      }
+      var movementFactor = 1;
+      if(limb.growing) {
+        movementFactor = 0.5;
+      }
+      var dVx0 = force / b0.mass * Math.cos(theta);
+      var dVy0 = force / b0.mass * Math.sin(theta);
+      var dVx1 = -force / b1.mass * Math.cos(theta) * movementFactor;
+      var dVy1 = -force / b1.mass * Math.sin(theta) * movementFactor;
+      b0.vel.x = Math.min( 20, Math.max( b0.vel.x + dVx0, -20 ));
+      b0.vel.y = Math.min( 20, Math.max( b0.vel.y + dVy0, -20 ));
+      b1.vel.x = Math.min( 20, Math.max( b1.vel.x + dVx1, -20 ));
+      b1.vel.y = Math.min( 20, Math.max( b1.vel.y + dVy1, -20 ));
+    }
+  }
+}
+
+function updateBodyPartPositions() {
+  for(var i = 0; i < Eves.length; i++) {
+    var eve = Eves[i];
+    for(var j = 0; j < eve.bodyParts.length; j++) {
+      var bodyPart = eve.bodyParts[j];
+
+      //set new positions:
+      bodyPart.pos.x += bodyPart.vel.x;
+      bodyPart.pos.y += bodyPart.vel.y;
+      
+      //redirect if hitting edge of screen:
+      if(bodyPart.pos.x <= bodyPart.mass || bodyPart.pos.x >= settings.width - bodyPart.mass) {
+        bodyPart.pos.x = limitPositions(bodyPart.pos.x, 1, bodyPart.mass)[0];
+        bodyPart.vel.x = -1 * bodyPart.vel.x;
+      }
+      if(bodyPart.pos.y <= bodyPart.mass || bodyPart.pos.y >= settings.height - bodyPart.mass) {
+        bodyPart.pos.y = limitPositions(1, bodyPart.pos.y, bodyPart.mass)[1];
+        bodyPart.vel.y = -1 * bodyPart.vel.y;
+      }
+
+      //render new body part positions:
+      d3.select('#eve' + eve.id + 'b' + j)
+        .attr('cx', bodyPart.pos.x).attr('cy', bodyPart.pos.y);
+    }
+
+    //render new limb positions:
+    for(var k = 0; k < eve.limbs.length; k++) {
+      var b0 = eve.bodyParts[eve.limbs[k].connections[0]];
+      var b1 = eve.bodyParts[eve.limbs[k].connections[1]];
+      d3.select('#eve' + eve.id + 'l' + k)
+        .attr('x1', b0.pos.x).attr('y1', b0.pos.y)
+        .attr('x2', b1.pos.x).attr('y2', b1.pos.y);
+    }
+  }
+}
+
+function killEves() {
+  var slowest = 0;
+  for(var i = 0; i < Eves.length; i++) {
+    var eveSpeed = (Eves[i].stats.distanceTraveled / Eves[i].stats.cyclesSinceBirth) || 0;
+    var smallestSpeed = (Eves[slowest].stats.distanceTraveled / Eves[slowest].stats.cyclesSinceBirth) || 0;
+    if(eveSpeed < smallestSpeed) {
+      slowest = i;
+    }
+  }
+
+  Eves.splice(slowest,1);
+
+  //Remove from board
+  var elementsToRemove = [];
+  var killEve = d3.select('.board').selectAll('.eve')
+    .data(Eves, function(d) { return d.id })
+    .exit()
+    .remove();
+
+  // var partsToRemove = killEve.selectAll('.part')[0];
+  // partsToRemove.push(killEve);
+  // var counter = partsToRemove.length;
+  // var removePiece = function(queue) {
+  //   var part = d3.select(queue.splice(0,1)[0]);
+  //   part.remove();
+  //   while(counter > 0) {
+  //     counter--;
+  //     setTimeout(function() {
+  //       removePiece(queue);
+  //     },100)
+  //   }
+  // }
+  
+  // removePiece(partsToRemove);
+    // .remove();
+  // would be great if i could remove a piece at a time
+
 }
 
 function deriveEveData(proto) {
@@ -227,147 +316,9 @@ function deriveEveData(proto) {
   //need to set stats.currentPos
   data.stats.currentPos = {x: data.bodyParts[0].pos.x, y: data.bodyParts[0].pos.y}
   
-return data;
+  return data;
 }
 
-function checkIfAllIncluded(connections, partCount) {
-  var nodes = {};
-  connections.forEach(function(conn) {
-    nodes[conn[0]] = true;
-    nodes[conn[1]] = true;
-  });
-  return Object.keys(nodes).length === partCount;
-}
-
-function checkIfConnected(array) {
-  var nodes = {};
-  var setInc = 1;
-  for(var i = 0; i < array.length; i++) {
-    var conns = array[i];
-    var leastSet = Math.min(nodes[conns[0]] || Infinity, nodes[conns[1]] || Infinity);
-    if(leastSet < Infinity) {
-      nodes[conns[0]] = leastSet;
-      nodes[conns[1]] = leastSet;
-    } else {
-      nodes[conns[0]] = setInc;
-      nodes[conns[1]] = setInc;
-      setInc++;
-    }
-  }
-  for(var set in nodes) {
-    if(nodes[set] !== 1) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function renderEves() {
-  var d3Eves = d3.select('.board').selectAll('.eve')
-    .data(Eves, function(d) { return d.id });
-  //new ones:
-  d3Eves.enter()
-    .append(drawEve);
-    //click handler for stats
-}
-
-function populateData() {
-  for(var i = 0; i < settings.eveCount; i ++) {
-    Eves.push(createEveData());
-  }
-}
-
-function animate() {
-  setInterval( function() {
-    applyLimbForces();
-    updateBodyPartPositions();
-  }, settings.stepTime);
-}
-
-function applyLimbForces() {
-  for(var i = 0; i < Eves.length; i++) {
-    var eve = Eves[i];
-    for(var j = 0; j < eve.limbs.length; j++) {
-      var limb = eve.limbs[j];
-      var b0 = eve.bodyParts[limb.connections[0]];
-      var b1 = eve.bodyParts[limb.connections[1]];
-      var displacement, force;
-      limb.currentLength = findDistance(b0.pos, b1.pos)
-      if(limb.growing) {
-        displacement = limb.maxLength - limb.currentLength;
-        force = displacement * 0.1 - 1.5;
-        if(limb.currentLength >= limb.maxLength) {
-          limb.growing = false;
-        }
-      } else {
-        displacement = limb.initialLength - limb.currentLength;
-        force = displacement * 0.1 + 1.5;
-        if(limb.currentLength <= limb.initialLength) {
-          limb.growing = true;
-        }
-      }
-      var xPosDiff = b1.pos.x - b0.pos.x;
-      var yPosDiff = b1.pos.y - b0.pos.y;
-      if(xPosDiff === 0) {
-        var theta = Math.PI;
-      }
-      else {
-        var theta = Math.atan(yPosDiff / xPosDiff);
-      }
-      if (xPosDiff >= 0) {
-        force *= -1;
-      }
-      var movementFactor = 1;
-      if(limb.growing) {
-        movementFactor = 0.5;
-      }
-      var dVx0 = force / b0.mass * Math.cos(theta);
-      var dVy0 = force / b0.mass * Math.sin(theta);
-      var dVx1 = -force / b1.mass * Math.cos(theta) * movementFactor;
-      var dVy1 = -force / b1.mass * Math.sin(theta) * movementFactor;
-      b0.vel.x = Math.min( 20, Math.max( b0.vel.x + dVx0, -20 ));
-      b0.vel.y = Math.min( 20, Math.max( b0.vel.y + dVy0, -20 ));
-      b1.vel.x = Math.min( 20, Math.max( b1.vel.x + dVx1, -20 ));
-      b1.vel.y = Math.min( 20, Math.max( b1.vel.y + dVy1, -20 ));
-    }
-  }
-}
-
-function updateBodyPartPositions() {
-  for(var i = 0; i < Eves.length; i++) {
-    var eve = Eves[i];
-    for(var j = 0; j < eve.bodyParts.length; j++) {
-      var bodyPart = eve.bodyParts[j];
-
-      //set new positions:
-      bodyPart.pos.x += bodyPart.vel.x;
-      bodyPart.pos.y += bodyPart.vel.y;
-      
-      //redirect if hitting edge of screen:
-      if(bodyPart.pos.x <= bodyPart.mass || bodyPart.pos.x >= settings.width - bodyPart.mass) {
-        bodyPart.pos.x = limitPositions(bodyPart.pos.x, 1, bodyPart.mass)[0];
-        bodyPart.vel.x = -1 * bodyPart.vel.x;
-      }
-      if(bodyPart.pos.y <= bodyPart.mass || bodyPart.pos.y >= settings.height - bodyPart.mass) {
-        bodyPart.pos.y = limitPositions(1, bodyPart.pos.y, bodyPart.mass)[1];
-        bodyPart.vel.y = -1 * bodyPart.vel.y;
-      }
-
-      //render new body part positions:
-      d3.select('#eve' + eve.id + 'b' + j)
-        .attr('cx', bodyPart.pos.x).attr('cy', bodyPart.pos.y);
-    }
-
-    //render new limb positions:
-    for(var k = 0; k < eve.limbs.length; k++) {
-      var b0 = eve.bodyParts[eve.limbs[k].connections[0]];
-      var b1 = eve.bodyParts[eve.limbs[k].connections[1]];
-      d3.select('#eve' + eve.id + 'l' + k)
-        .attr('x1', b0.pos.x).attr('y1', b0.pos.y)
-        .attr('x2', b1.pos.x).attr('y2', b1.pos.y);
-    }
-  }
-}
 
 function collectStats() {
   for(var i = 0; i < Eves.length; i++) {
@@ -378,44 +329,3 @@ function collectStats() {
     eve.stats.cyclesSinceBirth += 1;
   }
 }
-
-function killEves() {
-  var slowest = 0;
-  for(var i = 0; i < Eves.length; i++) {
-    var eveSpeed = (Eves[i].stats.distanceTraveled / Eves[i].stats.cyclesSinceBirth) || 0;
-    var smallestSpeed = (Eves[slowest].stats.distanceTraveled / Eves[slowest].stats.cyclesSinceBirth) || 0;
-    if(eveSpeed < smallestSpeed) {
-      slowest = i;
-    }
-  }
-
-  Eves.splice(slowest,1);
-
-  //Remove from board
-  var elementsToRemove = [];
-  var killEve = d3.select('.board').selectAll('.eve')
-    .data(Eves, function(d) { return d.id })
-    .exit()
-    .remove();
-
-  // var partsToRemove = killEve.selectAll('.part')[0];
-  // partsToRemove.push(killEve);
-  // var counter = partsToRemove.length;
-  // var removePiece = function(queue) {
-  //   var part = d3.select(queue.splice(0,1)[0]);
-  //   part.remove();
-  //   while(counter > 0) {
-  //     counter--;
-  //     setTimeout(function() {
-  //       removePiece(queue);
-  //     },100)
-  //   }
-  // }
-  
-  // removePiece(partsToRemove);
-    // .remove();
-  // would be great if i could remove a piece at a time
-
-}
-
-
